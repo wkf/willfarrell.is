d7b80be02a938e58eebef80d200a67e329bafb15
Will Farrell <will@mojotech.com>
2015-03-09 23:04:59 -0400

    Make the site a bit more responsive.

--- js/out/wkf/site.js
+++ js/out/wkf/site.js
@@ -3,30 +3,30 @@ goog.provide('wkf.site');
 goog.require('cljs.core');
 if(typeof wkf.site.site !== 'undefined'){
 } else {
-wkf.site.site = (function (){var G__29032 = new cljs.core.PersistentArrayMap(null, 1, [cljs.core.constant$keyword$20,false], null);
-return (cljs.core.atom.cljs$core$IFn$_invoke$arity$1 ? cljs.core.atom.cljs$core$IFn$_invoke$arity$1(G__29032) : cljs.core.atom.call(null,G__29032));
+wkf.site.site = (function (){var G__169746 = new cljs.core.PersistentArrayMap(null, 1, [cljs.core.constant$keyword$252,false], null);
+return (cljs.core.atom.cljs$core$IFn$_invoke$arity$1 ? cljs.core.atom.cljs$core$IFn$_invoke$arity$1(G__169746) : cljs.core.atom.call(null,G__169746));
 })();
 }
 /**
 * Start the site. Attempt to be idempotent.
 */
 wkf.site.start = (function start(){
-if(cljs.core.truth_(cljs.core.constant$keyword$20.cljs$core$IFn$_invoke$arity$1((function (){var G__29034 = wkf.site.site;
-return (cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(G__29034) : cljs.core.deref.call(null,G__29034));
+if(cljs.core.truth_(cljs.core.constant$keyword$252.cljs$core$IFn$_invoke$arity$1((function (){var G__169748 = wkf.site.site;
+return (cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(G__169748) : cljs.core.deref.call(null,G__169748));
 })()))){
 return null;
 } else {
-return cljs.core.swap_BANG_.cljs$core$IFn$_invoke$arity$4(wkf.site.site,cljs.core.assoc,cljs.core.constant$keyword$20,true);
+return cljs.core.swap_BANG_.cljs$core$IFn$_invoke$arity$4(wkf.site.site,cljs.core.assoc,cljs.core.constant$keyword$252,true);
 }
 });
 /**
 * Stop the site. Attempt to be idempotent. Useful for interactive local development.
 */
 wkf.site.stop = (function stop(){
-if(cljs.core.truth_(cljs.core.constant$keyword$20.cljs$core$IFn$_invoke$arity$1((function (){var G__29036 = wkf.site.site;
-return (cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(G__29036) : cljs.core.deref.call(null,G__29036));
+if(cljs.core.truth_(cljs.core.constant$keyword$252.cljs$core$IFn$_invoke$arity$1((function (){var G__169750 = wkf.site.site;
+return (cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(G__169750) : cljs.core.deref.call(null,G__169750));
 })()))){
-return cljs.core.swap_BANG_.cljs$core$IFn$_invoke$arity$4(wkf.site.site,cljs.core.assoc,cljs.core.constant$keyword$20,false);
+return cljs.core.swap_BANG_.cljs$core$IFn$_invoke$arity$4(wkf.site.site,cljs.core.assoc,cljs.core.constant$keyword$252,false);
 } else {
 return null;
 }
