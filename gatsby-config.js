d1dc1483bf84e5908f7b53401c5746ba94120c95
Will Farrell <wkfarrell@gmail.com>
2018-01-20 12:30:34 -0500

    Add favicon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,25 @@ module.exports = {
   siteMetadata: {
     title: 'Will Farrell',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/favicon.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
+  ],
 };
